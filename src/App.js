cf807ba7ff64e395f97380e2a6f51c7fbade349a
Massimiliano <masdelmon@hotmail.it>
2019-02-09 00:38:54 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom';
 import NavBar from './components/NavBar'
+import CoursesList from './components/CoursesList'
+
 class App extends Component {
   render() {
     return (
       <div>
         <NavBar />
+        <CoursesList />
       </div>
     )
   }
